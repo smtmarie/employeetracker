c458ab37b660240961ee055706c6f04ae93865e2
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-05-01 14:05:24 -0600

    fixing syntax for heroku upload

--- db/schema.sql
+++ db/schema.sql
@@ -1,35 +1,56 @@
-DROP DATABASE IF EXISTS employees_DB;
-CREATE DATABASE employees_DB;
+DROP DATABASE IF EXISTS employeeDB;
+CREATE DATABASE employeeDB;
 
-USE employees_DB;
+USE employeeDB;
 
-CREATE TABLE department(
-
-id INT NOT NULL,
-name VARCHAR(45),
-PRIMARY KEY(id)
+CREATE TABLE department
+(
+    id INT NOT NULL
+    AUTO_INCREMENT,
+name VARCHAR
+    (30),
+PRIMARY KEY
+    (id)
 );
 
-CREATE TABLE role(
-
-id INT NOT NULL,
-title VARCHAR(45),
-salary DECIMAL(10,2),
+    CREATE TABLE role
+    (
+        id INT NOT NULL
+        AUTO_INCREMENT,
+title VARCHAR
+        (30) NOT NULL,
+salary DECIMAL
+        (10,2), 
 department_id INT NOT NULL,
-PRIMARY KEY(id)
+PRIMARY KEY
+        (id)
 );
 
-CREATE TABLE employee(
-
-id INT NOT NULL,
-first_name VARCHAR(45),
-last_name VARCHAR(45),
+        CREATE TABLE employee
+        (
+            id INT NOT NULL
+            AUTO_INCREMENT,
+first_name VARCHAR
+            (30) NOT NULL,
+last_name VARCHAR
+            (30) NOT NULL,
 role_id INT NOT NULL,
-manager_id INT NOT NULL,
-PRIMARY KEY(id)
+manager_id INT NULL,
+PRIMARY KEY
+            (id)
 );
 
-SELECT * FROM department;
-SELECT * FROM role;
-SELECT * FROM employee;
+            SELECT *
+            FROM department;
+            SELECT *
+            FROM role;
+            SELECT *
+            FROM employee;
+
+
+
+
+
+
+
 
