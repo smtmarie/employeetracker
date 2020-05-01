59aed3cec6a982456d6030cc9c985bb6fe419acb
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-05-01 14:08:30 -0600

    syntax fixes

--- db/schema.sql
+++ db/schema.sql
@@ -1,51 +1,37 @@
-DROP DATABASE IF EXISTS employeeDB;
-CREATE DATABASE employeeDB;
+DROP DATABASE IF EXISTS employeesDB;
+CREATE DATABASE employeesDB;
 
-USE employeeDB;
+USE employeesDB;
 
 CREATE TABLE department
 (
-    id INT NOT NULL
-    AUTO_INCREMENT,
-name VARCHAR
-    (30),
-PRIMARY KEY
-    (id)
+id INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(30),
+PRIMARY KEY(id)
 );
 
-    CREATE TABLE role
-    (
-        id INT NOT NULL
-        AUTO_INCREMENT,
-title VARCHAR
-        (30) NOT NULL,
-salary DECIMAL
-        (10,2), 
+CREATE TABLE role
+(
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL(10,2), 
 department_id INT NOT NULL,
-PRIMARY KEY
-        (id)
+PRIMARY KEY(id)
 );
 
-        CREATE TABLE employee
-        (
-            id INT NOT NULL
-            AUTO_INCREMENT,
-first_name VARCHAR
-            (30) NOT NULL,
-last_name VARCHAR
-            (30) NOT NULL,
+CREATE TABLE employee
+(
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
 role_id INT NOT NULL,
 manager_id INT NULL,
-PRIMARY KEY
-            (id)
+PRIMARY KEY(id)
 );
 
-            SELECT *
-            FROM department;
-            SELECT *
-            FROM role;
-            SELECT *
-            FROM employee;
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
 
 
 
